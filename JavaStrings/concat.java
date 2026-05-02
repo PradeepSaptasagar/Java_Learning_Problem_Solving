0690ca7f9f75453ada0aaaa42fa83b703288b2da
Pradeep047-dev <pradeepsaptasagar2004@gmail.com>
2026-05-02 14:53:30 +0530

    Program to execute the concatenation of strings

---
+++ JavaStrings/concat.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class concat {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String firstName = sc.next();
+        String lastName = sc.next();
+        String fullName = firstName + "@" + lastName;
+        System.out.println(fullName.length());
+        // charAt
+        for (int i = 0; i < fullName.length(); i++) {
+            System.out.println(fullName.charAt(i));
+        }
+    }
+}
