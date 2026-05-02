da901dc4278129fb3b1a8b13bf304f96bf929a88
Pradeep047-dev <pradeepsaptasagar2004@gmail.com>
2026-05-02 13:35:22 +0530

    Program to print the arrays concept

---
+++ JavaArrays/arrays.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class arrays {
+    public static void main(String[] args) {
+        // int[] marks = new int[3];
+        // int marks[] = { 97, 98, 95 };
+        // marks[0] = 97;
+        // marks[1] = 98;
+        // marks[2] = 95;
+        // // System.out.println(marks[0]);
+        // System.out.println(marks[1]);
+        // System.out.println(marks[2]);
+        // for (int i = 0; i < 3; i++) {
+        // System.out.println(marks[i]);
+        // }
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int numbers[] = new int[size];
+        for (int i = 0; i < size; i++) {
+            numbers[i] = sc.nextInt();
+        }
+        for (int i = 0; i < size; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
