5397a43f4b089466c53fdcb8bcce242f101342fe
Pradeep047-dev <pradeepsaptasagar2004@gmail.com>
2026-05-02 14:10:49 +0530

    Program to execute loops concept

---
+++ Java_Loops/Loop.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Loop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Hello World");
+        }
+        sc.close();
+    }
+}
