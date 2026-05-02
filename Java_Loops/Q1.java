632c22b8f0d8eaed52ca225f7c6553ef12ab365d
Pradeep047-dev <pradeepsaptasagar2004@gmail.com>
2026-05-02 14:11:52 +0530

    Program to print the pattern using loop

---
+++ Java_Loops/Q1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Q1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < 11; i++) {
+            System.out.println(i);
+        }
+        sc.close();
+    }
+}
