37ca9fceb10972a9e86f5c6d77ac0a76e532a4cb
Pradeep047-dev <pradeepsaptasagar2004@gmail.com>
2026-05-02 14:50:53 +0530

    Code

---
+++ JavaVariables/NewCode.java
@@ -0,0 +1,7 @@
+public class NewCode {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java Bitches");
+        System.out.println("Now the real game begins");
+    }
+}
