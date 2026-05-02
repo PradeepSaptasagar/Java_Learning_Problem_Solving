0184880ddd1f8e491196dfe5a9e25abb0007e6ee
Pradeep047-dev <pradeepsaptasagar2004@gmail.com>
2026-05-02 13:04:35 +0530

    Program to compare the strings

---
+++ JavaStrings/stringcompare.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class stringcompare {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name1 = sc.next();
+        String name2 = sc.next();
+        if (name1.compareTo(name2) == 0) {
+            System.out.println("Strings are equal");
+        } else {
+            System.out.println("Strings are not equal");
+        }
+    }
+}
