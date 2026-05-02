dd9b7bed91ed443f55c712081d638e3c171aed1c
Pradeep047-dev <pradeepsaptasagar2004@gmail.com>
2026-05-02 15:01:28 +0530

    Program to calculate the sum of odd numbers

---
+++ Practice_Problems/sumoddnumbersQ2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.lang.*;
+
+public class sumoddnumbersQ2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        double sum = Math.pow((n + 1) / 2, 2);
+        System.out.println(sum);
+    }
+}
