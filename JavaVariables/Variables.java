fa7bf22bf9ad328698c1a4e69746c09cfac32620
Pradeep047-dev <pradeepsaptasagar2004@gmail.com>
2026-05-02 14:50:04 +0530

    Variables concepts

---
+++ JavaVariables/Variables.java
@@ -0,0 +1,10 @@
+public class Variables {
+    public static void main(String[] args) {
+        // variables
+        String name = "Pradeep";
+        int age = 9999;
+        double price = 420.23;
+        int a = 25;
+        int b = 10;
+    }
+}
