cf671b52794d119e0fc6921609dfc5057577d289
Pradeep047-dev <pradeepsaptasagar2004@gmail.com>
2026-05-02 14:57:29 +0530

    Program to immplement datatypes concept

---
+++ Practice_Problems/datatypes.java
@@ -0,0 +1,15 @@
+public class datatypes {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int sum = a + b;
+        // System.out.print(sum);
+        int diff = b - a;
+        System.out.println(diff);
+        int multi = a * b;
+        System.out.println(multi);
+        // quiz solution
+        int ans = (a * b) / (a - b);
+        System.out.println(ans);
+    }
+}
