78f477b0513c2566386cb3119d770f1235a7e537
Pradeep047-dev <pradeepsaptasagar2004@gmail.com>
2026-05-02 14:42:48 +0530

    Program to calculate the product of umbers by creating a separate function

---
+++ Java_Functions_and_Methods/product.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class product {
+    public static int calculateProduct(int a, int b) {
+        int product = a * b;
+        return product;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int product = calculateProduct(a, b);
+        System.out.println("Product of 2 nos is: " + product);
+    }
+}
