5702b1696957e8bee2a31a591061024d5e60029a
Pradeep047-dev <pradeepsaptasagar2004@gmail.com>
2026-05-02 13:49:37 +0530

    Program to implement the calculator

---
+++ Java_Conditional_Statements/JL3Q1.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class JL3Q1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int calc = sc.nextInt();
+        switch (calc) {
+            case 1:
+                System.out.println("Addition is:" + (a + b));
+                break;
+            case 2:
+                System.out.println("Subtraction is:" + (a - b));
+                break;
+            case 3:
+                System.out.println("Multiplication is:" + (a * b));
+                break;
+            case 4:
+                System.out.println("Division is:" + (a / b));
+                break;
+            case 5:
+                System.out.println("Modulo or Remainder is:" + (a % b));
+                break;
+            default:
+                System.out.println("Invalid");
+        }
+        sc.close();
+    }
+}
