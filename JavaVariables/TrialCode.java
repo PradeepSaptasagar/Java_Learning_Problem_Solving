413ca05c07c44c96f0d4280853549447400443e6
Pradeep047-dev <pradeepsaptasagar2004@gmail.com>
2026-05-02 14:51:20 +0530

    Trial running the code

---
+++ JavaVariables/TrialCode.java
@@ -0,0 +1,7 @@
+public class TrialCode {
+    public static void main(String[] args) {
+        System.out.print("Welcome to Java\n");
+        System.out.print("Welcome to Java Bitches\n");
+        System.out.print("Now the real game begins\n");
+    }
+}
