a33b153615c125e486e4bf98f73873f2a8cc3f3b
PradeepSaptasagar <pradeepsaptasagar2004@gmail.com>
2026-05-20 15:57:27 +0530

    Move all X to the end

---
+++ JavaRecursionAdvancedQA/MovAllXend.java
@@ -0,0 +1,24 @@
+public class MovAllXend {
+    public static void movAllX(String str, int idx, int count, String newString) {
+        if (idx == str.length()) {
+            for (int i = 0; i < count; i++) {
+                newString += 'x';
+            }
+            System.out.println(newString);
+            return;
+        }
+        char currChar = str.charAt(idx);
+        if (currChar == 'x') {
+            count++;
+            movAllX(str, idx + 1, count, newString);
+        } else {
+            newString += currChar;
+            movAllX(str, idx + 1, count, newString);
+        }
+    }
+
+    public static void main(String[] args) {
+        String str = "axbcxxd";
+        movAllX(str, 0, 0, "");
+    }
+}
